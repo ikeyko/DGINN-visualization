edaa3f4eb25395da90992c847d1505033eaba0cb
ikeyko <58029827+ikeyko@users.noreply.github.com>
2022-03-04 12:47:08 +0100

    Add files via upload

--- utils.R
+++ utils.R
@@ -1,6 +1,8 @@
 cleandf <- function(df){
   # recode all empty cells and na cells to true NA
+  print("start cleandf")
   df[df==""] <- NA
+  print("check methods")
   levels(df$BUSTED)[levels(df$BUSTED)=="na"] <- "N"
   levels(df$BppM1M2)[levels(df$BppM1M2)=="na"] <- "N"
   levels(df$BppM7M8)[levels(df$BppM7M8)=="na"] <- "N"
@@ -13,19 +15,21 @@ cleandf <- function(df){
   # df$codemlM1M2[df$codemlM1M2=="na"] <- "N"
   # df$codemlM7M8[df$codemlM7M8=="na"] <- "N"
   df[df=="na"] <- NA
-
+  print("row 18")
   # count methods saying Y and methods saying N
   df$nbY <- apply(df, 1, function(x) length(which(x=="Y")))
+  print("row 21")
   df$nbN <- apply(df, 1, function(x) length(which(x=="N")))
-
+  print("row 23")
   # calculate percentage of sites under positive selection
   cols <- grep("PSS", colnames(df))
+  print("row 26")
   df$MEME.PSS[df$BUSTED == "N"] <- NA
   df$UniqueSites <- apply(df[, cols], 1, function(x) toString(na.omit(x)))
   df$UniqueSites <- gsub(", ", "-", df$UniqueSites)
   df$UniqueSites <- lapply(df$UniqueSites, function(x) unique(unlist(strsplit(x, split = "-"))))
   df$nbUniqueSites <- lapply(df$UniqueSites, function(x) length(x))
-
+  print("row 32")
   # check if user has provided a RefSeqSize column and use it for percentage calculation
   if("RefSeqSize" %in% colnames(df) && is.numeric(df$RefSeqSize) && !is.na(df$RefSeqSize)){
     df$percentage <- as.numeric(df$nbUniqueSites)/df$RefSeqSize * 100
